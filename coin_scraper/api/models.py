a8120f3b4e546a501a3d5c0ef6ba008f0330276e
Madhvesh <madhvesh77@gmail.com>
2024-06-10 20:52:01 +0530

    define job task models

--- coin_scraper/api/models.py
+++ coin_scraper/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    job_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Task(models.Model):
+    job = models.ForeignKey(Job, related_name='tasks', on_delete=models.CASCADE)
+    coin = models.CharField(max_length=10)
+    output = models.JSONField(null=True, blank=True)
+    status = models.CharField(max_length=20, default='PENDING')
